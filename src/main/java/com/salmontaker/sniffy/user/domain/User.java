7d7e0306158cb5f004ed2b2a2509dabfe945dab6
Seungwon Lee <seungwon.dev@gmail.com>
2025-09-29 12:44:05 +0900

    chore: 유저 문자열 길이 수정

--- src/main/java/com/salmontaker/sniffy/user/domain/User.java
+++ src/main/java/com/salmontaker/sniffy/user/domain/User.java
@@ -1,10 +1,7 @@
 package com.salmontaker.sniffy.user.domain;
 
 import com.salmontaker.sniffy.common.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,8 +15,14 @@ public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(length = 255)
     private String email;
+
+    @Column(length = 255)
     private String password;
+    
+    @Column(length = 50)
     private String nickname;
 
     private User(String email, String password, String nickname) {
