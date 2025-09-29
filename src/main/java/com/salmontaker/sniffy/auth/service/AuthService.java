3d15788bf0f72393f304bab8bdc7d8cc5802792f
Seungwon Lee <seungwon.dev@gmail.com>
2025-09-29 15:09:31 +0900

    chore: 불필요한 Transactional 어노테이션 제거

--- src/main/java/com/salmontaker/sniffy/auth/service/AuthService.java
+++ src/main/java/com/salmontaker/sniffy/auth/service/AuthService.java
@@ -7,10 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class AuthService {
     private final UserRepository userRepository;
