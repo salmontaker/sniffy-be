2a98b1f86be40850e41f4e74cdcbef31411dcf59
Seungwon Lee <seungwon.dev@gmail.com>
2025-11-12 18:28:16 +0900

    chore: 토큰 폐기 기능을 위한 임시 엔드포인트 추가

--- src/main/java/com/salmontaker/sniffy/auth/controller/AuthController.java
+++ src/main/java/com/salmontaker/sniffy/auth/controller/AuthController.java
@@ -3,8 +3,11 @@ package com.salmontaker.sniffy.auth.controller;
 import com.salmontaker.sniffy.auth.dto.request.LoginRequest;
 import com.salmontaker.sniffy.auth.dto.response.LoginResponse;
 import com.salmontaker.sniffy.auth.service.AuthService;
+import com.salmontaker.sniffy.user.dto.response.UserResponse;
+import com.salmontaker.sniffy.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
+    private final UserService userService;
 
     @PostMapping("/login")
     public LoginResponse login(@Valid @RequestBody LoginRequest request) {
         return authService.login(request);
+    }
+
+    @PostMapping("/logout")
+    public UserResponse logout(@AuthenticationPrincipal Integer userId) {
+        return userService.getUser(userId);
     }
 }
