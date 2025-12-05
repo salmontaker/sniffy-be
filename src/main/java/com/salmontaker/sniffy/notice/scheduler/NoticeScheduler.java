360a1564eeef287daf448d9e5093061289fb65e8
Seungwon Lee <seungwon.dev@gmail.com>
2025-12-05 13:37:08 +0900

    refactor: 키워드 알림 생성 서비스 분리

--- src/main/java/com/salmontaker/sniffy/notice/scheduler/NoticeScheduler.java
+++ src/main/java/com/salmontaker/sniffy/notice/scheduler/NoticeScheduler.java
@@ -1,6 +1,6 @@
 package com.salmontaker.sniffy.notice.scheduler;
 
-import com.salmontaker.sniffy.notice.service.NoticeService;
+import com.salmontaker.sniffy.notice.service.NoticeCreateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -8,10 +8,10 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class NoticeScheduler {
-    private final NoticeService noticeService;
+    private final NoticeCreateService noticeCreateService;
 
     @Scheduled(cron = "0 0 6 * * *")
     public void createKeywordNotification() {
-        noticeService.createKeywordNotification();
+        noticeCreateService.createKeywordNotification();
     }
 }
--- src/main/java/com/salmontaker/sniffy/notice/service/NoticeService.java
+++ src/main/java/com/salmontaker/sniffy/notice/service/NoticeService.java
@@ -1,14 +1,9 @@
 package com.salmontaker.sniffy.notice.service;
 
 import com.salmontaker.sniffy.common.PageResponse;
-import com.salmontaker.sniffy.founditem.domain.FoundItem;
-import com.salmontaker.sniffy.founditem.repository.FoundItemRepository;
 import com.salmontaker.sniffy.notice.domain.Notice;
 import com.salmontaker.sniffy.notice.dto.response.NoticeResponse;
 import com.salmontaker.sniffy.notice.repository.NoticeRepository;
-import com.salmontaker.sniffy.user.domain.User;
-import com.salmontaker.sniffy.user.domain.UserKeyword;
-import com.salmontaker.sniffy.user.repository.UserKeywordRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -17,79 +12,14 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class NoticeService {
-    private final UserKeywordRepository userKeywordRepository;
-    private final FoundItemRepository foundItemRepository;
     private final NoticeRepository noticeRepository;
-
-    @Transactional
-    public void createKeywordNotification() {
-        Map<User, List<UserKeyword>> keywordsMap = getUserKeywordMap();
-        List<FoundItem> todayItems = findTodayItems();
-        List<Notice> notices = new ArrayList<>();
-
-        for (Map.Entry<User, List<UserKeyword>> entry : keywordsMap.entrySet()) {
-            User user = entry.getKey();
-            List<UserKeyword> userKeywords = entry.getValue();
-
-            for (UserKeyword keyword : userKeywords) {
-                List<FoundItem> matchedItems = findMatchedItems(todayItems, keyword.getKeyword());
-                if (matchedItems.isEmpty()) {
-                    continue;
-                }
-
-                notices.add(createNotification(user, keyword.getKeyword(), matchedItems));
-            }
-        }
-
-        noticeRepository.saveAll(notices);
-    }
-
-    private Map<User, List<UserKeyword>> getUserKeywordMap() {
-        return userKeywordRepository.findAll()
-                .stream()
-                .collect(Collectors.groupingBy(UserKeyword::getUser));
-    }
-
-    private List<FoundItem> findTodayItems() {
-        LocalDateTime startOfToday = LocalDate.now().atStartOfDay();
-        return foundItemRepository.findToday(startOfToday);
-    }
-
-    private List<FoundItem> findMatchedItems(List<FoundItem> items, String keyword) {
-        String lowerKeyword = keyword.toLowerCase();
-
-        return items.stream()
-                .filter(item -> item.getFdPrdtNm().toLowerCase().contains(lowerKeyword))
-                .toList();
-    }
-
-    private Notice createNotification(User user, String keyword, List<FoundItem> matched) {
-        String title = String.format("[키워드 알림] \"%s\"에 해당하는 물품 수: %d", keyword, matched.size());
-        String content = matched.stream()
-                .limit(5)
-                .map(this::summaryItem)
-                .collect(Collectors.joining("\n"));
-
-        return Notice.create(user, title, content);
-    }
-
-    private String summaryItem(FoundItem item) {
-        String url = String.format("https://www.lost112.go.kr/find/findDetail.do?ATC_ID=%s&FD_SN=%d", item.getAtcId(), item.getFdSn());
-        return String.format("%s(%s): %s", item.getAgency().getName(), item.getFdPrdtNm(), url);
-    }
-
+    
     public PageResponse<NoticeResponse> getNotices(Integer userId, Pageable pageable) {
         Page<Notice> notices = noticeRepository.findByUserId(userId, pageable);
         return PageResponse.from(notices.map(NoticeResponse::from));
